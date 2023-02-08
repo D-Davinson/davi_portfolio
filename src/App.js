4e352530d49d1fb2a72ecff6b905b22acce83ad6
Davinson <doglasprincedavinson@gmail.com>
2023-02-08 16:37:10 +0100

    About ny mis css and animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 
 function App() {
   return (
     <div>
       <Navbar />
         <Home />
+          <About />
     </div>
   );
 }
