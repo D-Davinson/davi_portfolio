9c1dc024cf111436c7614c4dd8ae318a9e315702
Davinson <doglasprincedavinson@gmail.com>
2023-02-11 21:05:16 +0100

    langages done/project-ny

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Langages from "./components/Langages";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div>
-    <Navbar />
-    <Home />
-    <About />
-    <Langages />
+      <Navbar />
+        <Home />
+          <About />
+            <Langages />
+              <Projects />
     </div>
   );
 }
