d446b3e2103d730981298d965aa97a2eaf215820
Davinson <doglasprincedavinson@gmail.com>
2023-02-12 14:36:56 +0100

    Everything done without Resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,25 @@
 import React, { useState } from 'react'
 import {FaBars} from "react-icons/fa";
 import { AiFillStar } from "react-icons/ai"
+import { Link } from "react-scroll"
 const Navbar = () => {
     //State
     const  [nav,setNav] = useState(false);
 
-    const links = [{id:1,link: "home"},{id:2,link: "about"},{id:3,link:"Langages"},{id:4,link: "Projects"},{id:5,link: "CV"},{id:6,link: 'Contact'},];
+    const links = [{id:1,link: "home"},{id:2,link: "About"},{id:3,link:"Langages"},{id:4,link: "Projects"},{id:5,link:"Resume"}];
 
   return (
-    <div className='flex justify-between z-10 w-full h20 p-3 text-white bg-purple-900 '>
+    <div name="Portfolio" className='flex justify-between z-10 w-full h20 p-3 text-white bg-purple-900 '>
     <div>
         <h1 className=" text-3xl ml-2 text-orange-500 underline">Portfolio</h1>
     </div>
     <ul className='hidden md:flex'>
 
+
         {links.map(({id,link}) => (
-        <li key={id} className='mx-9 px-2 py-2 cursor-pointer rounded-xl capitalize font-medium
-        text-lime-300 hover:scale-110 duration-200 hover:underline hover:bg-gray-600 hover:opacity-80 '>{link}</li>
+        <li  key={id} className='mx-9 px-2 py-2 cursor-pointer rounded-xl capitalize font-medium
+        text-lime-300 hover:scale-110 duration-200 hover:underline hover:bg-gray-600 hover:opacity-80'>
+            <Link to={link}smooth duration={500}>{link}</Link></li>
         ))}
     </ul>
     <div onClick={()=> setNav(!nav)} className='cursor-pointer pr-3 z-20 text-orange-500 hover:scale-110 duration-200 md:hidden'>
@@ -26,7 +29,7 @@ const Navbar = () => {
         <ul className=' flex flex-col justify-center items-center 
         absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-purple-900 to-orange-500 text-lime-100'>
         {links.map(({id,link}) => (
-            <li key={id} className='px-4 cursor-pointer capitalize py-6 text-3xl hover:scale-110 duration-200' >{link}</li>
+            <li key={id} className='px-4 cursor-pointer capitalize py-6 text-3xl hover:scale-110 duration-200'>{link}</li>
     ))}
         </ul>
   
