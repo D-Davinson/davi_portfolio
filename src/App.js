1403e4da33b0e3705cc43c3f9971c8e43432f1b0
Davinson <doglasprincedavinson@gmail.com>
2023-02-12 16:55:27 +0100

    try to deploy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Langages from "./components/Langages";
 import Projects from "./components/Projects";
-import Social_Link from "./components/Social_Link";
+import Socialink from "./components/Sociallink";
 import Footer from "./components/Footer";
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
           <About />
             <Langages />
               <Projects />
-                <Social_Link />
+                <Socialink />
                   <Footer />
     </div>
   );
